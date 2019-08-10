5b7c6de1b6f40a29ed10e2d1bec3f9cee8b05d4c
kiljoonho <kiljh@me.com>
2019-08-10 13:40:39 +0900

    2진수로 음수 만들기

---
+++ Chapter2/src/chapter2/BinaryTest.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int bNum = 0B1010;
+		int oNum = 012;
+		int xNum = 0XA;
+		
+		System.out.print(num);
+		System.out.print(bNum);
+		System.out.print(oNum);
+		System.out.print(xNum);
+	}
+
+}
